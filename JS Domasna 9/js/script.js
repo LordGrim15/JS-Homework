32bb9f12708d97cb3835bf03894fe20cd5971e74
Petar Gjorgjioski <58377922+LordGrim15@users.noreply.github.com>
2019-12-20 02:05:23 +0100

    Update script.js

--- JS Domasna 9/js/script.js
+++ JS Domasna 9/js/script.js
@@ -1,11 +1,5 @@
-let allParagraphs = document.querySelectorAll('p');
+let allParagraphsAndHeaders = document.querySelectorAll('p, h1, h3');
 for (let i = 0; i < allParagraphs.length; i++) {
 	allParagraphs[i].innerText = '';
-	allParagraphs[i].innerText = 'I changed the text of all paragraphs';
-}
-
-let allHeaders = document.querySelectorAll('h1, h3');
-for (let i = 0; i < allHeaders.length; i++) {
-	allHeaders[i].innerText = '';
-	allHeaders[i].innerText = 'I changed the text of all headers';
+	allParagraphs[i].innerText = 'I changed the text of both paragraphs and headers';
 }
